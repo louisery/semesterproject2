8178d9f78c8abdcac500841e823cf5374044ef6a
Louise Ryste <louiseryste@gmail.com>
2018-12-03 22:15:48 +0100

    Added the info from the API that should be used on the character cards, i.e. name, gender

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+/* All information about the game */
 function init() {
 	loadCharacters();
 
@@ -21,6 +23,8 @@ function init() {
 	}
 }
 
+/* Fetch characters from API */
+
 function loadCharacters() {
 	let characters = ['16', '27', '2024', '1880', '238', '867', '565', '583', '957', '954']
 
@@ -33,3 +37,44 @@ function loadCharacters() {
 		render(state);
 	});
 }
+
+/* Character Cards */
+function createCharacterCard(resultItem, selectPlayer1Function, selectPlayer2Function) {
+    var wrapper = document.createElement('div')
+    var character = document.getElementById('character');
+    var name = document.createElement('h3');
+    var gender = document.createElement('div');
+    var culture = document.createElement('div');
+    var born = document.createElement('div');
+    var died = document.createElement('div');
+    var titles = document.createElement('div');
+    var aliases = document.createElement('div');
+
+    name.innerHTML = resultItem.name;
+    gender.innerHTML = 'Gender: ' + resultItem.gender;
+    culture.innerHTML = 'Culture: ' + resultItem.culture;
+    born.innerHTML = 'Born: ' + resultItem.born;
+    died.innerHTML = 'Died: ' + resultItem.died;
+    titles.innerHTML = 'Titles: ' + resultItem.titles;
+    aliases.innerHTML = 'Aliases: ' + resultItem.aliases;
+
+    wrapper.className = 'characterCard';
+    wrapper.appendChild(name);
+    wrapper.appendChild(gender);
+    wrapper.appendChild(culture);
+    wrapper.appendChild(born);
+    wrapper.appendChild(died);
+    wrapper.appendChild(titles);
+    wrapper.appendChild(aliases);
+
+    var player1Button = document.createElement('button')
+    var player2Button = document.createElement('button')
+    player1Button.addEventListener("click", (e) => selectPlayer1Function(resultItem))
+    player1Button.innerText = "Select as player 1";
+    player2Button.addEventListener("click", (e) => selectPlayer2Function(resultItem))
+    player2Button.innerText = "Select as player 2";
+    wrapper.appendChild(player1Button);
+    wrapper.appendChild(player2Button);
+
+    return wrapper;
+  }
