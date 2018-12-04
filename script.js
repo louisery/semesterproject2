9f8021df876ce7a6bbd64f2598deb141c3e80b3a
Louise Ryste <louiseryste@gmail.com>
2018-12-04 13:04:44 +0100

    Added players and traps to the game board

--- script.js
+++ script.js
@@ -66,123 +66,138 @@ function renderCharacterSelect(state) {
 			el.appendChild(createCharacterCard(character, selectPlayer1Function, selectPlayer2Function));
 		});
 	}
-	}
+}
 
 
-	function renderGame(state) {
-		let el = document.getElementById('game');
-		el.innerHTML = '';
+function renderGame(state) {
+	let el = document.getElementById('game');
+	el.innerHTML = '';
 
-		// Canvas
-		let canvas = document.createElement('canvas');
-		canvas.height = 600;
-		canvas.width = 720;
-		canvas.className = 'gameBoard';
-		let ctx = canvas.getContext('2d');
+	// Canvas
+	let canvas = document.createElement('canvas');
+	canvas.height = 600;
+	canvas.width = 720;
+	canvas.className = 'gameBoard';
+	let ctx = canvas.getContext('2d');
 
-		// Draw canvas game board
-		for (var x = 0; x < 6; x++) {
-			for (var y = 0; < 5; y++) {
+	// Draw canvas game board
+	for (var x = 0; x < 6; x++) {
+		for (var y = 0; < 5; y++) {
 
-				if (x % 2 === y % 2) {
-					ctx.fillStyle = 'gray';
-					ctx.fillRect(120 * x, 120 * 7, 120, 120);
-				} else {
-					ctx.fillStyle = 'dimgray';
-					ctx.fillRect(120 * x, 120 * y, 120, 120);
-				}
-
-
-
+			// Every other square is gray/dimgray
+			if (x % 2 === y % 2) {
+				ctx.fillStyle = 'gray';
+				ctx.fillRect(120 * x, 120 * 7, 120, 120);
+			} else {
+				ctx.fillStyle = 'dimgray';
+				ctx.fillRect(120 * x, 120 * y, 120, 120);
 			}
+
+			let boardNumber = y * 6 + (x + 1);
+			if (state.traps[boardNumber] !== undefined) {
+				// Traps
+				ctx.fillStyle = 'red';
+				ctx.fillRect(120 * x, 120 * y, 120, 120);
+			}
+			if (boardNumber === state.posPlayer1) {
+				// Player 1
+				ctx.fillStyle = 'green';
+				ctx.fillRect(120 * x, 120 * y, 60, 60);
+			}
+			if (boardNumber === state.posPlayer2) {
+				// Player 2
+				ctx.fillStyle = 'blue';
+				ctx.fillRect(120 * x, 120 * y, 60, 60);
+			}
+
 		}
 	}
 
-	// Finale page
-	function renderFinale(state) {
-		let el = document.getElementById('game');
-		el.innerHTML = '';
+	el.appendChild(canvas);
+}
 
-		let winnerText = '';
-		if (state.posPlayer1 > state.posPlayer2) {
-			winnerText = 'Congratulations Player 1';
+// Finale page
+function renderFinale(state) {
+	let el = document.getElementById('game');
+	el.innerHTML = '';
+
+	let winnerText = '';
+	if (state.posPlayer1 > state.posPlayer2) {
+		winnerText = 'Congratulations Player 1';
+	} else {
+		winnerText = 'Congratulations Player 2';
+	}
+
+	el.innerHTML = 'Somebody won';
+	el.append('<h2>' + winnerText + '</h2>');
+
+}
+
+// Render options, which page to appear
+function render(state) {
+	if (state.gameStage === 'LOADING') {
+		renderLoading(state);
+	} else if (state.gameStage === 'CHARACTER_SELECT') {
+		renderCharacterSelect(state);
+	} else if (state.gameStage === 'GAME') {
+		renderGame(state);
+	} else if (state.gameStage === 'FINALE') {
+		renderFinale(state);
+	}
+}
+
+
+function game() {
+	state = init();
+
+	render(state);
+}
+
+// Dice
+function rollDice() {
+	return Math.floor(Math.random() * 6) + 1;
+}
+
+// Move according to dice roll
+function rollDiceAndMove() {
+	state.lastDiceRoll = rollDice();
+
+	if (state.nextPlayer === 'player1') {
+		state.posPlayer1 += state.lastDiceRoll;
+
+		//If the value is not equal to undefined it's a trap
+		if (state.traps[state.posPlayer1] !== undefined) {
+			console.log(state.traps[state.posPlayer1].trapInfo);
+			state.posPlayer1 -= state.traps[state.posPlayer1].backwardsValue
+		}
+		//If player1 has reached the max position(30) the Finale page appear
+		if (state.posPlayer1 >= state.maxPosition) {
+			state.gameStage = 'FINALE';
+		}
+		//If dice shows a number less than 6 it is player2's turn
+		if (state.lastDiceRoll < 6) {
+			state.nextPlayer = 'player2'
 		} else {
-			winnerText = 'Congratulations Player 2';
-		}
+			state.posPlayer2 += state.lastDiceRoll;
 
-		el.innerHTML = 'Somebody won';
-		el.append('<h2>' + winnerText + '</h2>');
-
-	}
-
-	// Render options, which page to appear
-	function render(state) {
-		if (state.gameStage === 'LOADING') {
-			renderLoading(state);
-		} else if (state.gameStage === 'CHARACTER_SELECT') {
-			renderCharacterSelect(state);
-		} else if (state.gameStage === 'GAME') {
-			renderGame(state);
-		} else if (state.gameStage === 'FINALE') {
-			renderFinale(state);
-		}
-	}
-
-
-	function game() {
-		state = init();
-
-		render(state);
-	}
-
-	// Dice
-	function rollDice() {
-		return Math.floor(Math.random() * 6) + 1;
-	}
-
-	// Move according to dice roll
-	function rollDiceAndMove() {
-		state.lastDiceRoll = rollDice();
-
-		if (state.nextPlayer === 'player1') {
-			state.posPlayer1 += state.lastDiceRoll;
-
-			//If the value is not equal to undefined it's a trap
-			if (state.traps[state.posPlayer1] !== undefined) {
-				console.log(state.traps[state.posPlayer1].trapInfo);
-				state.posPlayer1 -= state.traps[state.posPlayer1].backwardsValue
+			if (state.traps[state.posPlayer2] !== undefined) {
+				console.log(state.traps[state.posPlayer2].trapInfo);
+				state.posPlayer2 -= state.traps[state.posPlayer2].backwardsValue
 			}
-			//If player1 has reached the max position(30) the Finale page appear
-			if (state.posPlayer1 >= state.maxPosition) {
+
+			if (state.posPlayer2 >= state.maxPosition) {
 				state.gameStage = 'FINALE';
 			}
-			//If dice shows a number less than 6 it is player2's turn
 			if (state.lastDiceRoll < 6) {
-				state.nextPlayer = 'player2'
-			} else {
-				state.posPlayer2 += state.lastDiceRoll;
-
-				if (state.traps[state.posPlayer2] !== undefined) {
-					console.log(state.traps[state.posPlayer2].trapInfo);
-					state.posPlayer2 -= state.traps[state.posPlayer2].backwardsValue
-				}
-
-				if (state.posPlayer2 >= state.maxPosition) {
-					state.gameStage = 'FINALE';
-				}
-				if (state.lastDiceRoll < 6) {
-					state.nextPlayer = 'player1';
-				}
+				state.nextPlayer = 'player1';
 			}
-
-			render(state);
-
-
 		}
+
+		render(state);
+
+
 	}
-
-
-
+}
 
 // Character Cards
 function createCharacterCard(resultItem, selectPlayer1Function, selectPlayer2Function) {
@@ -224,3 +239,5 @@ function createCharacterCard(resultItem, selectPlayer1Function, selectPlayer2Fun
 
 	return wrapper;
 }
+
+game();
